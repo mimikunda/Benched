552f73865c23c7fd6e09004b2814b3182d7f90a5
Tibor  Urbančič <tibor.urbancic@students.vegova.si>
2026-05-31 00:19:01 +0200

    Initial commit

---
+++ app/src/main/java/com/mimikunda/benched/ui/screens/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.mimikunda.benched.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier){
+    Text("HEJ!")
+}
